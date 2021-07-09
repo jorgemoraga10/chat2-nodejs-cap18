818df51dafc238fc221454ca89493c656d0d4e4f
Jorge Moraga <jorgemoraga10@hotmail.com>
2021-07-09 14:57:56 -0400

    primer commit

---
+++ public/js/socket-chat.js
@@ -0,0 +1,70 @@
+var socket = io();
+
+
+
+
+const params =  new URLSearchParams( window.location.search );
+
+if ( !params.has('nombre') || !params.has('sala')) {
+    window.location = 'index.html';
+    throw new Error('El nombre y sala son necesarios');
+}
+
+const usuario = { 
+    nombre: params.get('nombre'),
+    sala: params.get('sala')
+};
+
+
+
+
+//escuchando cuando un socket se conecta 
+socket.on('connect', function() {
+    
+    console.log('Conectado al servidor');
+
+    socket.emit('entrar-Chat', usuario, function( resp ) {
+        console.log('Usuarios Conectados', resp);
+    });
+});
+
+
+
+
+// escuchancho cuando se desconecta un sockey 
+socket.on('disconnect', function() {
+    console.log('Perdimos conexión con el servidor');
+});
+
+
+
+
+// Enviar información
+// socket.emit('crear-mensaje', {
+//     usuario: 'Fernando',
+//     mensaje: 'Hola Mundo'
+// }, function(resp) {
+//     console.log('respuesta server: ', resp);
+// });
+
+
+
+
+// Escuchar información
+socket.on('crear-mensaje', function(mensaje) {
+    console.log('Servidor:', mensaje);
+});
+
+
+
+
+//Escuchar cuando un usuario entra o sale de las salas
+socket.on('lista-persona', function(personas) {
+    console.log( personas );
+});
+
+
+//accion de escuchar del cliente de un mensaje privado
+socket.on('mensaje-privado', function( mensaje ) {
+    console.log('Mensaje Privado:', mensaje );
+})
---
+++ server/sockets/socket.js
@@ -0,0 +1,63 @@
+const { io } = require('../server');
+const { Usuarios } = require('../classes/usuarios');
+const { crearMensaje } = require('../util/utilidades');
+
+
+
+
+const usuarios =  new Usuarios();
+
+io.on('connection', (client) => {
+
+    client.on('entrar-Chat', ( data, callback ) => {
+
+        if ( !data.nombre || !data.sala ) {
+            return callback({
+                error: true,
+                mensaje: 'El nombre/sala es necesario'
+            });
+        }
+
+        client.join(data.sala);
+
+        usuarios.agregarPersona(client.id, data.nombre, data.sala );
+
+        client.broadcast.to(data.sala).emit('lista-persona', usuarios.getPersonasPorSala(data.sala));
+        callback( usuarios.getPersonasPorSala(data.sala));
+    }); 
+    
+    
+
+    client.on('crear-mensaje', (data) => {
+
+        let persona = usuarios.getPersona(client.id);
+        let mensaje = crearMensaje( persona.nombre, data.mensaje );
+
+        client.broadcast.to(persona.sala).emit('crear-mensaje', mensaje );
+    })
+
+
+
+
+    client.on('disconnect', () => {
+
+        let personaBorrada = usuarios.borrarPersona( client.id );
+
+        client.broadcast.to(personaBorrada.sala).emit('crear-mensaje', crearMensaje('Administrador',`${personaBorrada.nombre} salió`));
+        client.broadcast.to(personaBorrada.sala).emit('lista-persona', usuarios.getPersonasPorSala(personaBorrada.sala));         
+    });
+             
+    
+
+    
+    //lo que hara el servidor cuando alguien quiere mandar un mensaje privado
+    client.on('mensaje-privado', (data) => {
+
+        let persona = usuarios.getPersona( client.id );
+        client.broadcast.to(data.para).emit('mensaje-privado', crearMensaje( persona.nombre, data.mensaje ));
+    });
+
+
+
+
+});
